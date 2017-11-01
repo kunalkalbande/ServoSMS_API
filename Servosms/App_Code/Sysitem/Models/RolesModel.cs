9e8b06242fe60c3fabb8d6058f68b28e0593ca40
kunalkalbande <dineshr@IDTP362>
2017-11-01 17:45:45 +0530

    Added RolesModel

---
+++ Servosms/App_Code/Sysitem/Models/RolesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servo_API.Models
+{
+    public class RolesModel
+    {
+        public int Role_ID { get; set; }
+        public string Role_Name { get; set; }
+        public string Description { get; set; }
+    }
+}
