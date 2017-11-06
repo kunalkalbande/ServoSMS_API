2b9db6956b86402edc990c685d7d4054c6d8c3d2
kunalkalbande <dineshr@IDTP362>
2017-11-06 16:48:36 +0530

    Added Model for Stock Adjustment

---
+++ Servosms/App_Code/Sysitem/Models/StockAdjustmentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servo_API.Models
+{
+    public class StockAdjustmentModel
+    {
+        public string Route_ID { get; set; }
+
+        public string Route_Name { get; set; }
+
+        public string Route_Km { get; set; }
+
+        public string Index_Route_Name { get; set; }
+
+        public string Selected_Route_Name { get; set; }
+    }
+}
